d77d05335aa0bc7ae2c37eaded3cfdd0b5aa87fe
Benjamin Garza <benjisgarza@gmail.com>
2019-09-23 15:16:05 -0700

    Added bootstrap and scss file

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,13 +8,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
@@ -29,7 +29,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
   
-  test "should get contact" do 
+  test "should get contact" do
     get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
